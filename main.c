b4b20d56ac8c1adbe14f923dfac1e3554db3f661
ANTONIO C. DOS S. SCARTEZZINI <16104006@l1801224.portoalegre.pucrsnet.br>
2016-06-08 10:09:23 -0300

    add base config loading

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
 #include "includes.h"
+#include "libconfig.h"
 
 int main(void) {
-  
+    config_t cfg, *cf;
+    const config_setting_t *retries;
+    const char *base = NULL;
+    int count, n, enabled;
+
+    cf = &cfg;
+    config_init(cf);
+
+    if (!config_read_file(cf, "game.cfg")) {
+        fprintf(stderr, "%s:%d - %s\n",
+            config_error_file(cf),
+            config_error_line(cf),
+            config_error_text(cf));
+        config_destroy(cf);
+        return -1;
+ }
 
  return 0;
 
