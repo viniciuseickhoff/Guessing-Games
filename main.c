212c6b44027fc640793d79d3a7ae682c3b9def17
ANTONIO C. DOS S. SCARTEZZINI <16104006@l1801224.portoalegre.pucrsnet.br>
2016-06-08 10:30:10 -0300

    Create constants for Language

--- main.c
+++ main.c
@@ -1,16 +1,25 @@
 #include "includes.h"
-#include "libconfig.h"
-
+#include "libconfig/lib/libconfig.h"
+#include "constants.h"
+#define EN 1
+#define PT 2
+#define getLanguage(a) a == 1 ? EN : PT
 int main(void) {
+    int l = 0;
+    printf("Choose your Language | Escolha sua Lingua");
+    printf("\n1- English (USA)\n");
+    printf("2- Portugues (BR)");
+    scanf("%d",&l);
+    #define CURR_LANG getLanguage(l)
     config_t cfg, *cf;
     const config_setting_t *retries;
     const char *base = NULL;
     int count, n, enabled;
-
+    
     cf = &cfg;
     config_init(cf);
 
-    if (!config_read_file(cf, "game.cfg")) {
+    if (!config_read_file(cf, "lang.cfg")) {
         fprintf(stderr, "%s:%d - %s\n",
             config_error_file(cf),
             config_error_line(cf),
@@ -18,6 +27,9 @@ int main(void) {
         config_destroy(cf);
         return -1;
  }
+  LANG *lang;
+  
+  
 
  return 0;
 
