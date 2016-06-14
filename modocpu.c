f7d51588167c6d6b38e5d67bb4dbe0a76b19ec3f
VINICIUS EICKHOFF <16110457@l1847029.portoalegre.pucrsnet.br>
2016-06-14 15:02:18 -0300

    trab

--- modocpu.c
+++ modocpu.c
@@ -2,19 +2,19 @@
 
 int initGameMode()
 {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	LANG *lang;
+	srand(time(NULL));
+	int x = rand()%100 + 1	
+	int resp;
+	while(true){
+	display(lang-> ola);
+	display(lang -> primeiraguess);
+	scanf("%d", resp);
+		if (resp == x)
+		{
+			display(lang-> consegui);
+			break;
+		}
+	}
 	return 0;
 }
